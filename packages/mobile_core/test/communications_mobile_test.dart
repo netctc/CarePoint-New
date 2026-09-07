5ba32c0687998e2c1079e8bc98b9f73ef2e89b9a
netctc <36329039+netctc@users.noreply.github.com>
2026-09-07 07:54:54 +0300

    test: use Flutter at-least widget matcher

--- packages/mobile_core/test/communications_mobile_test.dart
+++ packages/mobile_core/test/communications_mobile_test.dart
@@ -107,6 +107,6 @@ void main() {
     await tester.tap(find.text('Notifications'));
     await tester.pumpAndSettle();
     expect(find.text('Notification preferences'), findsOneWidget);
-    expect(find.text('Secure messages'), findsAtLeast(1));
+    expect(find.text('Secure messages'), findsAtLeastNWidgets(1));
   });
 }
