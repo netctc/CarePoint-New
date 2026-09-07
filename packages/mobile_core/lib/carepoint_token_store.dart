3d497dc2826137eb7225e54d04987a3581b1e1e9
netctc <36329039+netctc@users.noreply.github.com>
2026-09-07 11:45:23 +0300

    feat: add secure mobile token store abstraction

---
+++ packages/mobile_core/lib/carepoint_token_store.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+abstract interface class CarePointTokenStore {
+  Future<String?> readAccessToken();
+  Future<String?> readRefreshToken();
+  Future<void> writeTokens({required String accessToken, required String refreshToken});
+  Future<void> clear();
+}
+
+class SecureCarePointTokenStore implements CarePointTokenStore {
+  SecureCarePointTokenStore({FlutterSecureStorage? storage}) : _storage = storage ?? const FlutterSecureStorage();
+
+  static const _accessKey = 'carepoint.auth.access_token';
+  static const _refreshKey = 'carepoint.auth.refresh_token';
+  final FlutterSecureStorage _storage;
+
+  @override
+  Future<String?> readAccessToken() => _storage.read(key: _accessKey);
+
+  @override
+  Future<String?> readRefreshToken() => _storage.read(key: _refreshKey);
+
+  @override
+  Future<void> writeTokens({required String accessToken, required String refreshToken}) async {
+    await _storage.write(key: _accessKey, value: accessToken);
+    await _storage.write(key: _refreshKey, value: refreshToken);
+  }
+
+  @override
+  Future<void> clear() async {
+    await Future.wait([
+      _storage.delete(key: _accessKey),
+      _storage.delete(key: _refreshKey),
+    ]);
+  }
+}
+
+class MemoryCarePointTokenStore implements CarePointTokenStore {
+  String? accessToken;
+  String? refreshToken;
+
+  @override
+  Future<String?> readAccessToken() async => accessToken;
+
+  @override
+  Future<String?> readRefreshToken() async => refreshToken;
+
+  @override
+  Future<void> writeTokens({required String accessToken, required String refreshToken}) async {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
+
+  @override
+  Future<void> clear() async {
+    accessToken = null;
+    refreshToken = null;
+  }
+}
