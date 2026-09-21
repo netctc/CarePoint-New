3f2422d7951df69f1fa71cb8277b905d6f228a6e
netctc <36329039+netctc@users.noreply.github.com>
2026-09-21 04:20:12 +0300

    feat(v2): wire emergency contacts module

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -22,6 +22,7 @@ import { ConsentModule } from "./modules/consent/consent.module";
 import { ClinicalGovernanceModule } from "./modules/clinical-governance/clinical-governance.module";
 import { DataGovernanceModule } from "./modules/data-governance/data-governance.module";
 import { DependentsModule } from "./modules/dependents/dependents.module";
+import { EmergencyContactsModule } from "./modules/emergency-contacts/emergency-contacts.module";
 import { RefillModule } from "./modules/refill/refill.module";
 import { ReferralsModule } from "./modules/referrals/referrals.module";
 import { DocumentsModule } from "./modules/documents/documents.module";
@@ -73,6 +74,7 @@ const isolatedSyntheticPilot = isolatedSyntheticPrivatePilotActive(process.env);
     ReferralsModule,
     DataGovernanceModule,
     DependentsModule,
+    EmergencyContactsModule,
     ProvidersModule,
     ProviderCategoryFormsModule,
     ProviderWorkflowModule,
